a53a9fb82e6e4f0a6ea0983c34ccb873715da70a
Steven Weng <steven.weng@shopee.com>
2022-07-23 16:58:55 +0800

    feet: add source code files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./scss/main.scss";
+
+const App = (props) => {
+  return <div>App</div>;
+};
+
+App.propTypes = {};
+
+export default App;
